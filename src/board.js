2e5f99e69b5a52cf534185562f77b990594eb1b6
Lars <lars@harhatt.com>
2013-03-23 21:39:50 +0100

    refactoring toString method and collision checks to handle matrix representations

--- src/board.js
+++ src/board.js
@@ -12,31 +12,12 @@ function Board(width, height) {
 Board.prototype.toString = function() {
   var str = "";
   
-  if(this.hasFalling()) {
-    if(this.falling.type) {
-      //single block
-      var x = this.falling.xpos;
-      var y = this.falling.ypos;
-      this.matrix[y][x] = this.falling;  
-    } else {
-      //multiple block shapes
-      var pos = this.falling.blockPos();
-      for(var i = 0; i < pos.length; i++) {
-        var p = pos[i];
-        this.matrix[p.ypos][p.xpos] = p;
-      }
-    }
-  }
-  for(var b = 0; b < this.blocks.length; b++) {
-    var x = this.blocks[b].xpos;
-    var y = this.blocks[b].ypos;
-    this.matrix[y][x] = this.blocks[b];
-  }
-  
   for(var y = 0; y < this.height; y++) {
     for(var x = 0; x < this.width; x++) {
       var block = this.matrix[y][x];
-      if(block == null) {
+      if(this.hasFalling() && this.falling.xpos == x && this.falling.ypos == y) {
+        str = str + this.falling;
+      } else if(block == undefined) {
         str = str + ".";
       } else {
         str = str + block;
@@ -67,19 +48,30 @@ Board.prototype.drop = function(block) {
 }
 
 Board.prototype.tick = function() {
-  if(this.collisionCheck() || this.falling.collisionCheck(this.blocks)) {
-    this.blocks.push(this.falling);
+  if(this.boardCollision() || this.blockCollision()) {
+    var x = this.falling.xpos;
+    var y = this.falling.ypos;
+    this.matrix[y][x] = this.falling;
     this.falling = null;
   } else { 
     this.falling.moveDown();
   }
 }
-Board.prototype.collisionCheck = function() {
+Board.prototype.boardCollision = function() {
   //hit bottom of board?
   //compare to height - 1 as array is 0-indexed
   if(this.falling.ypos == (this.height -1)) {
     return true;
   }
 
+  return false;
+}
+
+Board.prototype.blockCollision = function() {
+  var x = this.falling.xpos;
+  var y = this.falling.ypos;
+  if(this.matrix[y+1][x]) {
+    return true;
+  }
   return false;
 }
